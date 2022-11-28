bd26a0623abcfc8a2471b26b5e321ebb1cddfe27
Alberto Mercado <amercadoarriola@gmail.com>
2022-11-28 18:01:09 +0100

    controller_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+
+  def create
+    @post = Post.new(params_post)
+    if @post.save
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+
+  private
+
+  def params_post
+    params.require(:post).permit(:title, :content, :membership_id)
+  end
 end
