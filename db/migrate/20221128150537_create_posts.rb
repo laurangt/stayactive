eed6626a92e6db67c8d61a912a8f1b8219f6c0a2
Laura Nguyen Trong <laurangt@yahoo.com>
2022-11-28 16:18:20 +0100

    model group, membership, post, comment

---
+++ db/migrate/20221128150537_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+      t.references :membership, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
