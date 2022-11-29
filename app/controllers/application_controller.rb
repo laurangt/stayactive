e736b653fe57be451fc91c5d816dd2dc1e8fcd40
Maria Villalonga <mvillalongacdv@gmail.com>
2022-11-29 11:40:16 +0100

    profil pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :bio, :photo])
+  end
 end
