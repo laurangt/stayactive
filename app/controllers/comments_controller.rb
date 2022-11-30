cad5bf00d54dcab99668ea78b3cfa5be866733cb
Laura Nguyen Trong <laurangt@yahoo.com>
2022-11-30 10:34:26 +0100

    asd

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to group_path
+  end
+
   private
 
   def comment_params
