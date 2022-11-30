342a89923c78d476acc40c18c9791e8432b9255c
Laura Nguyen Trong <laurangt@yahoo.com>
2022-11-30 15:57:35 +0100

    group authorization

---
+++ app/policies/group_policy.rb
@@ -0,0 +1,12 @@
+class GroupPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+end
