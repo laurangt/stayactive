37842d8396a61bc2447a84e2ee5f9a7c5d27a342
Laura Nguyen Trong <laurangt@yahoo.com>
2022-12-03 14:45:47 +0100

    seed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :hearts, dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 25 }
+  validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 200 }
 
   has_one_attached :photo
