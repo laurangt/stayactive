ad278f739a4cf6c1eb002eb57c656883572564c0
Laura Nguyen Trong <laurangt@yahoo.com>
2022-12-08 09:37:25 +0100

    sharepolicies

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,6 +1,7 @@
 class Log < ApplicationRecord
   belongs_to :goal
   has_one :project, through: :goal
+  has_one :user, through: :goal
   validates :content, presence: true
 
   has_one_attached :photo
